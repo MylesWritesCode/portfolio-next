93c8687ef5036e0dec4e25dd54607ce6290d4670
Myles Berueda <myles@polypixel.io>
2024-10-06 02:36:32 -1000

    feat(resume): adds about me (#3)
    * adds first about me section
    
    * fixes width for modal
    
    * adds momo pictures :)
    
    * adds about me

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,10 +8,20 @@ import { ThemeSwitcher } from '@/components/layout';
 
 const Header = () => {
   return (
-    <div className='sticky top-0 right-0 left-0 flex w-full flex-col items-center justify-center border-base-content border-b bg-base-100 px-4'>
-      <div className='flex w-full max-w-7xl flex-col items-center justify-between px-4 py-2 md:flex-row md:px-12'>
+    <div
+      className={cn(
+        'sticky top-0 right-0 left-0 z-50',
+        'border-base-content border-b',
+        'flex w-full flex-col items-center justify-center bg-base-100 px-4',
+      )}>
+      <div
+        className={cn(
+          'flex w-full flex-col items-center justify-between',
+          'max-w-7xl px-4 py-2',
+          'md:flex-row md:px-12',
+        )}>
         <div className='flex w-full justify-between md:w-auto'>
-          <h2 className='flex items-center font-bold text-2xl tracking-tight md:text-4xl md:tracking-tighter'>
+          <h2 className={cn('flex items-center font-bold text-2xl tracking-tight', 'md:text-4xl md:tracking-tighter')}>
             <NextLink href='/' className='hover:underline'>
               myles BERUEDA
             </NextLink>
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -23,7 +23,7 @@ export const Modal = React.forwardRef<ModalRef, ModalProps>(({ id, children, onC
 
   return (
     <dialog id={id} ref={modalRef} className='modal modal-bottom sm:modal-middle' onClose={handleOnClose}>
-      <div className='modal-box !max-w-[80%]'>
+      <div className='modal-box !max-w-[70%]'>
         {children}
         <div className='mt-4 flex items-center justify-center'>
           <span className='text-base-content text-xs italic'>
